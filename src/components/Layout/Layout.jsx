287687d0760c90650d5627a5c3944aa4764294f3
Prathameshgadale <prathameshgadale03@gmail.com>
2024-12-22 17:28:35 +0530

    Second commit

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import Header from '../Header/Header'
 import Routers from '../../router/Routers'
 import Footer from '../Footer/Footer'
+import Highlights from '../Highlights/Highlights'
+
 const Layout = () => {
   return (
     <>
    <Header/>
       <Routers/>
+      <Highlights />
       <Footer/>
     </>
   )
